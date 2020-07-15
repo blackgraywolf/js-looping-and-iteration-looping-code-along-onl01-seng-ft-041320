8a94e374237554f3c2b9f2abc10d7f88bbb2efde
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-15 20:01:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+
+function countDown(count) {
+    let i = 0
+
+    while (count >= 0) {
+        console.log(count)
+        count--
+    }
+} 
